b73da9247bb203ab5d7605215d87bdba06d9aad0
S-MunSoo <godmsoo93@gmail.com>
2022-04-09 02:05:55 +0900

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const searchEl = document.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
+const badgeEl = document.querySelector("header .badges");
 
 searchEl.addEventListener("click", function () {
   searchInputEl.focus();
@@ -14,3 +15,26 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+// 날개 배너가 스크롤되면 사라지게 되는 함수 호출 Window는 창을 의미한다
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // 배지 숨기기
+      // gsap.to(요소,지속시간,옵션);
+
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // 배지 보이지
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+      });
+    }
+  }, 300)
+);
+// _.throttle(함수,시간)
