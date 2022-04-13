636d40063bef88797c1b62890d5b3d62a02f3b71
S-MunSoo <godmsoo93@gmail.com>
2022-04-13 15:52:53 +0900

    공통 모듈 분리

---
+++ common.js
@@ -0,0 +1,19 @@
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+searchEl.addEventListener("click", function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener("focus", function () {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+// blur 반대되는 속성 focus 가 해재될 경우
+searchInputEl.addEventListener("blur", function () {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", "");
+});
+
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear(); // 2022 날짜 확인
--- main.js
+++ main.js
@@ -1,22 +1,6 @@
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
 const badgeEl = document.querySelector("header .badges");
 const toToEl = document.querySelector("#to-top");
 const fadeEl = document.querySelectorAll(".visual .fade-in");
-
-searchEl.addEventListener("click", function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener("focus", function () {
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색");
-});
-// blur 반대되는 속성 focus 가 해재될 경우
-searchInputEl.addEventListener("blur", function () {
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
-});
 
 // 날개 배너가 스크롤되면 사라지게 되는 함수 호출 Window는 창을 의미한다
 window.addEventListener(
@@ -148,6 +132,3 @@ spyELs.forEach(function (spyEl) {
     .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear();
